b04a077b90b0b0cf7251456dc7f5a51ba39a7a00
BartoszOrnaf <bartosz.ornaf@gmail.com>
2020-04-29 14:02:58 +0200

    Deleted comments

--- src/store.js
+++ src/store.js
@@ -1,9 +1,5 @@
-import { createStore } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-import appReducer from './reducers';
 import configureStore from './configureStore';
 
 const store = configureStore({});
-// const store = createStore(appReducer, composeWithDevTools());
 
 export default store;
